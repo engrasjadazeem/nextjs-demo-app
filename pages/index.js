c84ac861b22ab43f05919913d66e75b6dc575362
engrasjadazeem <engrasjadazeem@gmail.com>
2019-06-17 15:54:17 +0200

    First commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// This is the Link API
+import Layout from '../components/layout';
+
+export default function Index() {
+  return (<div>
+    <Layout>
+      <p>Hello Next.js</p>
+    </Layout>
+  </div>);
+}
+
