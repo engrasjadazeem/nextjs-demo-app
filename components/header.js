c38f197329b82be56771c49477d65c4d75b14da1
engrasjadazeem <engrasjadazeem@gmail.com>
2019-06-19 09:54:39 +0200

    Bootstrap added

--- components/header.js
+++ components/header.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 const linkStyle = {
   marginRight: 15
@@ -6,6 +7,14 @@ const linkStyle = {
 
 const Header = () => (
   <div>
+    <Head>
+    <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossOrigin="anonymous"
+      />
+    </Head>
     <Link href="/">
       <a style={linkStyle}>Home</a>
     </Link>
